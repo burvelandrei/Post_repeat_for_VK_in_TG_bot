2359917face68ef6780b303dc78f69706d16508f
Andrei Burvel <burvelandrei@gmail.com>
2024-02-08 22:16:37 +0300

    добавлен_шедулер

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 import logging
 import asyncio
+from datetime import datetime
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
 from handlers import other_handlers, user_handlers
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from service.service import get_wall_vk
 
 
 logger = logging.getLogger(__name__)
@@ -22,6 +25,10 @@ async def main():
 
     bot = Bot(config.tg_bot.token)
     dp = Dispatcher()
+
+    scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
+    scheduler.add_job(get_wall_vk, trigger='interval', seconds=5, start_date=datetime.now(), kwargs={'bot': bot})
+    scheduler.start()
 
     dp.include_router(user_handlers.rt)
     dp.include_router(other_handlers.rt)
