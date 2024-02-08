20f0f4cedaa24d8964bc0849ce0d480f7f4aba3e
Andrei Burvel <burvelandrei@gmail.com>
2024-02-08 19:48:48 +0300

    добавил_вк_данные

--- config_data/config.py
+++ config_data/config.py
@@ -9,13 +9,24 @@ class TgBot:
 
 
 @dataclass
+class VkPars:
+    token: str
+    privat_key: str
+    service_key: str
+
+
+@dataclass
 class Config:
     tg_bot: TgBot
+    vk_pars: VkPars
 
 
 def load_config(path: str|None = None) -> Config:
     env = Env()
     env.read_env(path)
     return Config(
-        tg_bot=TgBot(token=env("BOT_TOKEN"),
-    ))
+        tg_bot=TgBot(token=env("BOT_TOKEN")),
+        vk_pars=VkPars(token=env("VK_TOKEN"),
+        privat_key=env("PRIVATE_KEY"),
+        service_key=env("SERVICE_KEY")
+        ))
